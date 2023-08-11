697604a0d68abb5ebf3d9bf62429778df31dea11
ech0devv <winterarctic26@outlook.com>
2023-08-11 17:42:01 -0400

    finish writing update function 2/2

--- update.c
+++ update.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include "trim.h"
+
+// sauce: curl docs
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
 
 int update(int argc, char **argv) {
+  curl_global_init(CURL_GLOBAL_ALL);
   printf("updating repos...\n");
 
   FILE *galaxiesptr = fopen("/etc/luna/galaxies.conf", "r");
@@ -23,7 +33,23 @@ int update(int argc, char **argv) {
   };
   fclose(galaxiesptr);
   for (int z = 0; z < i; ++z) {
+    const char* filename = strrchr(repos[z], '/')+1;
+    char basePath[72] = "/etc/luna/galaxies/";
     printf("getting %d/%d\n", z + 1, i);
+    FILE *fp = fopen(strcat(basePath, filename), "wb");
+    CURL *easy = curl_easy_init();
+    curl_easy_setopt(easy, CURLOPT_URL, trim(repos[z]));
+    curl_easy_setopt(easy, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(easy, CURLOPT_WRITEDATA, fp);
+    CURLcode res = curl_easy_perform(easy);
+    if(res != CURLE_OK){
+      printf("curl: failed to download %s with %s", filename, curl_easy_strerror(res));
+      perror("failed to get repo");
+      return 2;
+    }
+    fclose(fp);
+    curl_easy_cleanup(easy);
   }
+  curl_global_cleanup();
   return 0;
 }
