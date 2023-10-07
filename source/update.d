3cc63b457ff466468646ce91fe896b069c7d1ce7
ech0devv <winterarctic26@outlook.com>
2023-10-07 12:17:33 -0400

    add repo update function

---
+++ source/update.d
@@ -0,0 +1,25 @@
+module update;
+
+import std.stdio : writefln;
+import std.file : readText, exists, mkdirRecurse;
+import std.string : strip, split;
+import std.path : baseName;
+import std.net.curl : download;
+
+import main;
+
+void updateRepos(string[] args) {
+    if (!exists("/etc/luna/repos.conf")) {
+        logger.fatal("cannot access '/etc/luna/repos.conf': no such file or directory");
+    }
+    if(!exists("/var/lib/luna/repos.conf.d/")){
+        mkdirRecurse("/var/lib/luna/repos.conf.d/");
+    }
+    logger.log("updating repos...");
+    string[] repos = split(strip(readText("/etc/luna/repos.conf")), "\n");
+    for(int i = 0; i < repos.length; ++i){
+        string fname = baseName(repos[i]);
+        writefln("updating %s (%s/%s)", fname, i+1, repos.length);
+        download(repos[i], "/var/lib/luna/repos.conf.d/" ~ fname);
+    }
+}
