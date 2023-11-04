c8cba8ae220121aacea3ce48eff63d34010fb0cf
ech0devv <winterarctic26@outlook.com>
2023-11-04 13:32:45 -0400

    start working on installing, remove strip from the dub.json

--- source/install.d
+++ source/install.d
@@ -11,6 +11,8 @@ void installPackage(string[] args) {
     if (packages.length != 1)
         logger.fatal(format("%s packages with name %s", packages.length == 0 ? "found no" : "found too many", args[1]));
     Lpkg pkg = packages[0];
+    //TODO make this actually work
+    logger.info("calculating deps...");
     logger.info(format("installing %s/%s::%s", pkg.loc.get.constellation, pkg.name, pkg
             .tag));
 }
