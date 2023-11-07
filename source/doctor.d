f3192259ec8ad7d3c459d2a3c54317dab12e916b
ech0devv <winterarctic26@outlook.com>
2023-11-07 17:52:58 -0500

    add destdir to doctor

--- source/doctor.d
+++ source/doctor.d
@@ -4,26 +4,35 @@ import std.file : exists, mkdirRecurse;
 import std.path : expandTilde;
 import std.net.curl : download;
 import std.format : format;
+import std.getopt : getopt, config;
 import logger;
 import update;
 
-void runDoctor() {
+void runDoctor(string[] args) {
     int problems = 0;
+    string destDir = "";
+    getopt(
+        "destdir", "destination to make repairs", &destDir,
+        config.noBundling,
+        config.stopOnFirstNonOption,
+        config.passThrough
+    );
     immutable string[] dirs = [
-        "/var/log/luna/", "/var/lib/luna/repos.conf.d/", "/etc/luna/", "/usr/src/luna/", "/var/lib/luna/installed.d/"
+        "/var/log/luna/", "/var/lib/luna/repos.conf.d/", "/etc/luna/",
+        "/usr/src/luna/", "/var/lib/luna/installed.d/"
     ];
     foreach (dir; dirs) {
         if (!exists(dir)) {
             ++problems;
-            logger.info(format("creating %s", expandTilde(dir)));
-            mkdirRecurse(expandTilde(dir));
+            logger.info(format("creating %s", destDir ~ dir));
+            mkdirRecurse(destDir ~ dir);
         }
 
     }
     if (!exists("/etc/luna/repos.conf")) {
         ++problems;
         logger.info("downloading default repos.conf and updating");
-        download("https://raw.githubusercontent.com/luna-lnx/repo/main/repos.conf.defaults", "/etc/luna/repos.conf");
+        download("https://raw.githubusercontent.com/luna-lnx/repo/main/repos.conf.defaults", destDir ~ "/etc/luna/repos.conf");
     }
     updateRepos([]);
     info(format("doctor: fixed %s issues", problems));
