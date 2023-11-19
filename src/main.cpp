e6b6408d14916c83fa39f925e6d5a3f76685ccbc
ech0 <winterarctic26@outlook.com>
2023-11-19 09:06:35 -0500

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#define VERS "v0.1"
-
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
-void initLogger(){
+#define VERS "v0.1"
+
+void initLogger()
+{
     // totally not stolen from https://github.com/gabime/spdlog/issues/290
     std::vector<spdlog::sink_ptr> sinks;
     sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("/var/log/luna/log.txt", INT32_MAX, 3));
@@ -13,7 +14,8 @@ void initLogger(){
     spdlog::register_logger(std::make_shared<spdlog::logger>("default", begin(sinks), end(sinks)));
 }
 
-int main() {
+int main()
+{
     // initialize the logger
     initLogger();
     spdlog::get("default")->info("luna {}", VERS);
