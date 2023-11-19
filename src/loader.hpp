8d09fe18aca21e29b627886f0dc98441f4b43314
ComputerKing12 <cpuking12@gmail.com>
2023-11-19 17:14:33 -0500

    add clang-format and add header guard

--- src/loader.hpp
+++ src/loader.hpp
@@ -1,13 +1,15 @@
+#pragma once
+
 #include <atomic>
 
 class Loader
 {
-private:
-    char* taskName;
-    void (*task)(Loader&);
-    std::atomic<bool> stopping = false;
-    void doLoader();
+  public:
+	Loader(char *taskName, void (*task)(Loader &));
 
-public:
-    Loader(char* taskName, void (*task)(Loader&));
+  private:
+	char *taskName;
+	void (*task)(Loader &);
+	std::atomic<bool> stopping = false;
+	void doLoader();
 };
