cf1dae66e36e009079b9907ef8a0ce031bea408d
ComputerKing12 <cpuking12@gmail.com>
2023-11-19 17:26:10 -0500

    updated loader to use std::cout instead of printf

--- src/loader.cpp
+++ src/loader.cpp
@@ -1,35 +1,36 @@
+#include "loader.hpp"
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
 #include <string>
 #include <thread>
-#include <atomic>
-#include <stdio.h>
-#include <iostream>
-#include <chrono>
-#include "loader.hpp"
 
 void Loader::doLoader()
 {
-    std::thread loader([this](std::atomic<bool>& stopping)
-                       {
-            const char* icons[4] = {"\\", "|", "/", "-"};
-            while(!stopping){
-                for(int i = 0; i < 4; ++i){
-                    printf("\r%s %s", icons[i], taskName);
-                    std::cout << std::flush;
-                    std::this_thread::sleep_for(std::chrono::milliseconds(150));
-                }
-            }
-            printf("\r%s... done!\n", taskName);
-            std::cout << std::flush; },
-                       std::ref(stopping));
+	std::thread loader(
+		[this](std::atomic<bool> &stopping) {
+			const char *icons[4] = {"\\", "|", "/", "-"};
+			while (!stopping)
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					std::cout << "\r" << icons[i] << " " << taskName << std::flush;
+					std::this_thread::sleep_for(std::chrono::milliseconds(150));
+				}
+			}
+			std::cout << "\r" << taskName << "... done!" << std::flush << std::endl;
+		},
+		std::ref(stopping));
 
-    task(*this);
-    stopping.store(true);
-    loader.join();
+	task(*this);
+	stopping.store(true);
+	loader.join();
 }
 
-Loader::Loader(char* taskName, void (*task)(Loader &))
+Loader::Loader(char *taskName, void (*task)(Loader &))
 {
-    this->taskName = taskName;
-    this->task = task;
-    doLoader();
+	this->taskName = taskName;
+	this->task = task;
+	doLoader();
 }
