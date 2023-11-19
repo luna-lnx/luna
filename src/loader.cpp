c515419fa38ccbbe572fe6f8bda0a6102a88e192
ech0 <winterarctic26@outlook.com>
2023-11-19 17:44:12 -0500

    add using namespace bit to all cpp files

--- src/loader.cpp
+++ src/loader.cpp
@@ -6,24 +6,27 @@
 #include <string>
 #include <thread>
 
+using namespace std;
+
+
 void Loader::doLoader()
 {
-    std::thread loader(
-        [this](std::atomic<bool> &stopping) {
+    thread loader(
+        [this](atomic<bool> &stopping) {
             const char *icons[4] = {"\\", "|", "/", "-"};
             while (!stopping)
             {
                 for (int i = 0; i < 4; ++i)
                 {
                     printf("\r%s %s", icons[i], taskName);
-                    std::cout << std::flush;
-                    std::this_thread::sleep_for(std::chrono::milliseconds(150));
+                    cout << flush;
+                    this_thread::sleep_for(chrono::milliseconds(150));
                 }
             }
             printf("\r%s... done!\n", taskName);
-            std::cout << std::flush;
+            cout << flush;
         },
-        std::ref(stopping));
+        ref(stopping));
 
     task(*this);
     stopping.store(true);
--- src/parseargs.cpp
+++ src/parseargs.cpp
@@ -3,22 +3,24 @@
 #include <deque>
 #include <string>
 
-Arg::Arg(std::string names, void (*func)(std::deque<std::string>))
+using namespace std;
+
+Arg::Arg(string names, void (*func)(deque<string>))
 {
     this->names = names;
     this->func = func;
 };
 
-void ParseArgs::addArgument(std::string names, void (*func)(std::deque<std::string>))
+void ParseArgs::addArgument(string names, void (*func)(deque<string>))
 {
     arguments.push_back(Arg(names, func));
 }
 
-void ParseArgs::parseArgs(std::deque<std::string> argsin)
+void ParseArgs::parseArgs(deque<string> argsin)
 {
     for (int i = 0; i < arguments.size(); ++i)
     {
-        std::deque<std::string> indivargs = split(arguments.at(i).names, "|");
+        deque<string> indivargs = split(arguments.at(i).names, "|");
         for (int j = 0; j < indivargs.size(); ++j)
         {
             if (argsin[0] == indivargs[j])
