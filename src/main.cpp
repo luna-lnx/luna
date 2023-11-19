1b88cf114885fc4ec969162c4ac946ddc5f4958d
ech0 <winterarctic26@outlook.com>
2023-11-19 08:59:46 -0500

    add flto, add logger

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,20 @@
 #define VERS "v0.1"
 
 #include "spdlog/spdlog.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+void initLogger(){
+    // totally not stolen from https://github.com/gabime/spdlog/issues/290
+    std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("/var/log/luna/log.txt", INT32_MAX, 3));
+    sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    spdlog::register_logger(std::make_shared<spdlog::logger>("default", begin(sinks), end(sinks)));
+}
 
 int main() {
-    spdlog::info("luna {}", VERS);
+    // initialize the logger
+    initLogger();
+    spdlog::get("default")->info("luna {}", VERS);
     return 0;
 }
