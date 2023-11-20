598033ae9c2070fd89a256b22e7edcfb8487602c
ech0 <winterarctic26@outlook.com>
2023-11-20 16:29:26 -0500

    stuff 2: electric boogaloo

--- include/lutils.hpp
+++ include/lutils.hpp
@@ -4,7 +4,18 @@
 #include <sstream>
 #include <string>
 
-std::deque<std::string> splitstr(std::string in, std::string del);
+std::deque<std::string> splitstr(std::string in, std::string del)
+{
+    std::deque<std::string> out;
+    size_t pos = 0;
+    while ((pos = in.find(del)) != std::string::npos)
+    {
+        out.push_front(in.substr(0, pos));
+        in.erase(0, pos + del.length());
+    }
+    out.push_front(in);
+    return out;
+}
 
 template <typename T> std::string sstr(const T &val)
 {
