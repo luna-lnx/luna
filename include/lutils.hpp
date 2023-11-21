b0cf67be4fe6b0422cbde944346ba38f5665ead4
ech0 <winterarctic26@outlook.com>
2023-11-20 21:09:59 -0500

    finish adding bg gradient

--- include/lutils.hpp
+++ include/lutils.hpp
@@ -7,7 +7,7 @@
 std::deque<std::string> splitstr(std::string in, std::string del);
 std::string color(u_int8_t r, u_int8_t g, u_int8_t b);
 std::string colorBg(u_int8_t r, u_int8_t g, u_int8_t b);
-std::string gradient(std::string input, u_int8_t fro[3], u_int8_t to[3]);
+std::string gradient(std::string input, u_int8_t fro[3], u_int8_t to[3], bool bg);
 std::string bold();
 std::string colorTerminate();
 // what does this do again lmao
