24d3281d5f8a048d61bba25249ff5dd4f8eeb550
ech0 <winterarctic26@outlook.com>
2023-11-21 19:37:06 -0500

    check if repoout is open

--- src/update.cpp
+++ src/update.cpp
@@ -1,13 +1,13 @@
-#include "loader.hpp"
-#include "lutils.hpp"
-#include "logger.hpp"
 #include "cpr/cpr.h"
+#include "loader.hpp"
+#include "logger.hpp"
+#include "lutils.hpp"
 #include <chrono>
 #include <deque>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <thread>
-#include <iostream>
-#include <fstream>
 
 namespace update
 {
@@ -15,18 +15,28 @@ void updateRepos(std::deque<std::string> args)
 {
     Loader ld("updating repos", [](Loader &l) {
         std::ifstream reposListFile("/etc/luna/repos.conf");
-        if(reposListFile.is_open()){
+        if (reposListFile.is_open())
+        {
             std::string tmp;
             reposListFile >> tmp;
             std::deque<std::string> reposList = splitstr(tmp, "\n");
-            for(int i = 0; i < reposList.size(); ++i){
-                l.setProgress(format("{}/{}", i+1, reposList.size()));
+            for (int i = 0; i < reposList.size(); ++i)
+            {
+                l.setProgress(format("{}/{}", i + 1, reposList.size()));
                 cpr::Response r = cpr::Get(cpr::Url{reposList.at(i)});
-                std::ofstream repoOut(format("/var/lib/luna/repos.d/{}", r.url.str().substr(r.url.str().find_last_of("/") + 1)));
-                repoOut << r.text;
-                repoOut.close();
-            }  
-        }else{
+                std::ofstream repoOut(
+                    format("/var/lib/luna/repos.d/{}", r.url.str().substr(r.url.str().find_last_of("/") + 1)));
+                if (repoOut.is_open())
+                {
+                    repoOut << r.text;
+                    repoOut.close();
+                }else{
+                    log(LogLevel::FATAL, "repoOut not open");
+                }
+            }
+        }
+        else
+        {
             log(LogLevel::FATAL, "reposListFile not open");
         }
         reposListFile.close();
