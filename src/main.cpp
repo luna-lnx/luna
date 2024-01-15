1ea24f8e8b61378317be8dac37a0c56e44a8f98e
ech0devv <winterarctic26@outlook.com>
2024-01-15 17:41:57 -0500

    i've made a monster

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@
 
 int main(int argc, char *argv[])
 {
+	std::deque<std::string> arguments(argv + 1, argv + argc);
 	if (getuid() != 0)
 	{
-		throw std::runtime_error("missing superuser permissions");
+		log(LogLevel::WARN, "missing permissions. attempting to rerun as root...");
+		system(format("su -c \"{} {}\"", argv[0], joinstr(arguments, " ")).c_str());
+		exit(0);
 	}
 	log(LogLevel::INFO, "luna - {}", VERS);
-	std::deque<std::string> arguments(argv + 1, argv + argc);
 	ParseArgs pa;
 	pa.addArgument("-u|--update|update", "updates the repos", &update::updateRepos);
 	pa.addArgument("-d|--doctor|doctor", "performs a lunapm health check", &doctor::runDoctor);
