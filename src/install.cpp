80a5e387994a0ae6f3fbb0e7db5c11df8e954f93
ech0devv <winterarctic26@outlook.com>
2024-02-26 20:41:01 -0500

    Do some dependency stuffs

--- src/install.cpp
+++ src/install.cpp
@@ -18,11 +18,3 @@ void install(std::string name)
 {
 }
 } // namespace install
-
-/*i am blanking rn
-idek how i should do this
-ima organize my thoughts here for the time being:
-first and foremost, what format should the package be in?
-bash would be optimal but it'd also be weird to parse to variables for dependency resolution (unless there's a library
-for that, i'll look)
-*/
