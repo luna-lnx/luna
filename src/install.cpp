632716fd803fe4ce4b8f568889cf4776486cf7e4
ech0devv <winterarctic26@outlook.com>
2024-02-26 21:54:58 -0500

    wgat

--- src/install.cpp
+++ src/install.cpp
@@ -7,12 +7,6 @@ namespace install
 {
 void installPackage(std::vector<std::string> args)
 {
-	ParseArgs pa;
-	std::string packagename;
-	pa.addArgument("-i|--install|install|-S", "installs a package", &packagename);
-	pa.setFlags(ParseArgs::CHECK_UNRECOGNIZED);
-	pa.parseArgs(args);
-	privEsc();
 }
 void install(std::string name)
 {
