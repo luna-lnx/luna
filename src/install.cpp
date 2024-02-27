89c5d16a1c8d8635f6e220a92b008ff0a3d69777
ech0devv <winterarctic26@outlook.com>
2024-02-26 20:04:12 -0500

    add note

--- src/install.cpp
+++ src/install.cpp
@@ -13,6 +13,16 @@ void installPackage(std::vector<std::string> args)
 	pa.setFlags(ParseArgs::CHECK_UNRECOGNIZED);
 	pa.parseArgs(args);
 	privEsc();
-	log(LogLevel::DEBUG, packagename);
+}
+void install(std::string name)
+{
 }
 } // namespace install
+
+/*i am blanking rn
+idek how i should do this
+ima organize my thoughts here for the time being:
+first and foremost, what format should the package be in?
+bash would be optimal but it'd also be weird to parse to variables for dependency resolution (unless there's a library
+for that, i'll look)
+*/
